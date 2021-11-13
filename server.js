0c01025725687e1bc554fe9d70c025fa3ea95fe9
TNTatOffice <tmoscovich@vidazoo.com>
2021-11-13 18:18:11 +0200

    updated v2

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const PORT = 3000;
 app.use('/', express.static('public'));
 
 
-// route to home page- get
+// route to home page
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
-
-
+// route to sign in page
+app.get('/login', (req,res) => {
+  res.sendFile(__dirname + '/signIn.html');
+});
 
 // server active
 app.listen(PORT);
