de9a5841e0f1db0efd3332c9d881033d15719b46
TNTatOffice <tmoscovich@vidazoo.com>
2021-11-14 16:35:42 +0200

    v7

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require ('mongoose');
+bodyParser = require("body-parser");
 app.use("/", express.static("public"));
 
 // route to home page
@@ -18,6 +20,25 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
   return res.sendFile(__dirname + "/signUp.html");
 });
+
+// mongoose URL connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/SVBurgerDB'); 
+const db = mongoose.connection;
+db.once('open', () => {console.log('SVBurgerDB is connected')});
+db.on('error', console.log.bind(console,'error in DB connection'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// creating post 
+app.post('/api/user', (req,res) => {
+const saveUser = new userSchema(req.body)
+saveUser.save((error,savedUser) => {
+if(error) throw error
+res.json(savedUser)
+})
+})
 
 // server active
 app.listen(PORT);
