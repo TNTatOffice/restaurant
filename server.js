d180339d9be0e68c93a9c5f9b911ff14309b37e3
TNTatOffice <tmoscovich@vidazoo.com>
2021-11-14 15:30:45 +0200

    v5

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
 // creating localhost on port 3000
-const express = require ('express');
-const { LEGAL_TCP_SOCKET_OPTIONS } = require('mongodb');
-const app = express()
+const express = require("express");
+const mongo = require("mongodb");
+const app = express();
 const PORT = 3000;
-app.use('/', express.static('public'));
-
+app.use("/", express.static("public"));
 
 // route to home page
-app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/login.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
 
 // route to sign in page
-app.get('/login', (req,res) => {
-    return res.sendFile(__dirname + '/signIn.html');
+app.get("/login", (req, res) => {
+  return res.sendFile(__dirname + "/signIn.html");
 });
 
-function redirect(){
-  const loginClick = document.getElementById('login');
-  const signUpClick = document.getElementById('signUp');
-  if(loginClick.clicked == true)
-  {
-    return res.render('/login')
-  }
-  else if (signUpClick.clicked == true)
-  {
-    return res.redirect('signUp.html')
-  }
-};
-
-
+// route to sign up page
+app.get("/signup", (req, res) => {
+  return res.sendFile(__dirname + "/signUp.html");
+});
 
 // server active
 app.listen(PORT);
